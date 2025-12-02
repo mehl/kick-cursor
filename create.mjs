435c7bed2ead6270113244ef5b2afa42d0bff73d
Bastian Frank <git@bastian-frank.de>
2025-12-02 11:52:04 +0100

    create size versions

--- create.mjs
+++ create.mjs
@@ -6,43 +6,75 @@ import { config } from './config.mjs';
 
 const exec = promisify(child_process.exec);
 
-
-const SCALE = 4;
-const FINAL_SIZE = 96;
-const MOVE_X = 0;
-const MOVE_Y = 0;
-const MOVE_SHADOW_X = 0;
-const MOVE_SHADOW_Y = 0;
-const BLUR = 2;
-const PADDING = BLUR * 2;
+const sizes = [
+    {
+        name: "4k",
+        scale: 4,
+        finalSize: 96,
+        moveX: 0,
+        moveY: 0,
+        moveShadowX: 0,
+        moveShadowY: 0,
+        blur: 2,
+        padding: 4
+    },
+    {
+        name: "2k",
+        scale: 2,
+        finalSize: 48,
+        moveX: 0,
+        moveY: 0,
+        moveShadowX: 0,
+        moveShadowY: 0,
+        blur: 2,
+        padding: 4
+    },
+    {
+        name: "1k",
+        scale: 1,
+        finalSize: 24,
+        moveX: 0,
+        moveY: 0,
+        moveShadowX: 0,
+        moveShadowY: 0,
+        blur: 1,
+        padding: 2
+    }
+];
 
 const IMG_DEST_PATH = "dist/img/";
-const XCURSOR_DEST_PATH = "dist/kick-cursor/";
+const THEME_DEST_PATH = "dist/BitplaneCursor";
 
 const SRC_PATH = "designs/";
 
-async function resizeAndShadow(fileName) {
+const themeDefinition = `[Icon Theme]
+Name=BitplaneCursor{suffix}
+Example=left_ptr
+Inherits=core
+`;
+
+async function resizeAndShadow(fileName, sizeConfig) {
     const image = sharp(fileName);
     const m = await image.metadata();
     const width = m.width || 1;
     const height = m.height || 1;
 
-    const resized = image.clone().resize(width * SCALE, height * SCALE, { kernel: 'nearest' });
+    const resized = image.clone().resize(width * sizeConfig.scale, height * sizeConfig.scale, { kernel: 'nearest' });
     const withBorder = resized.clone().extend({
-        top: PADDING,
-        bottom: PADDING,
-        left: PADDING,
-        right: PADDING,
+        top: sizeConfig.padding,
+        bottom: sizeConfig.padding,
+        left: sizeConfig.padding,
+        right: sizeConfig.padding,
         background: "#00000000"
     });
 
     const alpha = await withBorder.clone().extractChannel('alpha').toBuffer();
     // Linear does not directly work on alpha channels, so we need to convert it to a grayscale image first
-    const shadowMask = sharp(alpha).blur(BLUR).linear(0.3, 0);
+    const shadowMask = sharp(alpha).blur(sizeConfig.blur).linear(0.3, 0);
 
     const finalSize = {
-        width: width * SCALE + PADDING * 2,
-        height: height * SCALE + PADDING * 2
+        width: width * sizeConfig.scale + sizeConfig.padding * 2,
+        height: height * sizeConfig.scale + sizeConfig.padding * 2
     }
     console.log("Size from ", width, height, "to", finalSize);
 
@@ -63,11 +95,11 @@ async function resizeAndShadow(fileName) {
             background: { r: 0, g: 0, b: 0, alpha: 0 }
         }
     }).composite([
-        { input: await shadow.toBuffer(), left: MOVE_SHADOW_X, top: MOVE_SHADOW_Y },
-        { input: await resized.toBuffer(), left: MOVE_X, top: MOVE_Y },
+        { input: await shadow.toBuffer(), left: sizeConfig.moveShadowX, top: sizeConfig.moveShadowY },
+        { input: await resized.toBuffer(), left: sizeConfig.moveX, top: sizeConfig.moveY },
     ]).extend({
-        right: Math.max(FINAL_SIZE - finalSize.width, 0),
-        bottom: Math.max(FINAL_SIZE - finalSize.height, 0),
+        right: Math.max(sizeConfig.finalSize - finalSize.width, 0),
+        bottom: Math.max(sizeConfig.finalSize - finalSize.height, 0),
         background: "#00000000"
     }).toFormat("png");
 
@@ -75,8 +107,8 @@ async function resizeAndShadow(fileName) {
         {
             left: 0,
             top: 0,
-            width: FINAL_SIZE,
-            height: FINAL_SIZE
+            width: sizeConfig.finalSize,
+            height: sizeConfig.finalSize
         }
     );
 
@@ -85,21 +117,31 @@ async function resizeAndShadow(fileName) {
 
 (async () => {
     fs.mkdirSync(IMG_DEST_PATH, { recursive: true });
-    fs.mkdirSync(XCURSOR_DEST_PATH, { recursive: true });
-    for (var [key, value] of Object.entries(config)) {
-        console.log(key, value);
-        const image = await resizeAndShadow(SRC_PATH + value.file + '.png');
-        await image.toFile(IMG_DEST_PATH + key + '.png');
-        const hotspox = Math.min(FINAL_SIZE - 1, value.x * SCALE + MOVE_X);
-        const hotpoty = Math.min(FINAL_SIZE - 1, value.y * SCALE + MOVE_Y);
-        fs.writeFileSync(IMG_DEST_PATH + key + '.cursor', `${FINAL_SIZE} ${hotspox} ${hotpoty} ${IMG_DEST_PATH + key}.png\n`);
 
-        const cmd = `xcursorgen ${IMG_DEST_PATH + key + '.cursor'} ${XCURSOR_DEST_PATH + key}`;
-        console.log(cmd);
-        const { stdout, stderr } = await exec(cmd);
-        console.log('stdout:', stdout);
-        console.error('stderr:', stderr);
-
-    };
+    for (const sizeConfig of sizes) {
+        const themePath = THEME_DEST_PATH + `-${sizeConfig.name}/cursors/`;
+        fs.mkdirSync(themePath, { recursive: true });
+        for (var [key, value] of Object.entries(config)) {
+            // console.log(key, value);
+            const xcursorConfig = [];
+            const { scale, finalSize, moveX, moveY } = sizeConfig;
+            const fileName = IMG_DEST_PATH + key + `@${sizeConfig.name}.png`;
+            const image = await resizeAndShadow(SRC_PATH + value.file + '.png', sizeConfig);
+            await image.toFile(fileName);
+            const hotspox = Math.min(finalSize - 1, value.x * scale + moveX);
+            const hotpoty = Math.min(finalSize - 1, value.y * scale + moveY);
+            xcursorConfig.push(`${finalSize} ${hotspox} ${hotpoty} ${fileName}\n`);
+            fs.writeFileSync(IMG_DEST_PATH + key + '.cursor', xcursorConfig.join(''), 'utf8');
+            const finalFileName = themePath + key;
+            const cmd = `xcursorgen ${IMG_DEST_PATH + key + '.cursor'} ${finalFileName}`;
+            console.log(cmd);
+            const { stdout, stderr } = await exec(cmd);
+            console.log('stdout:', stdout);
+            console.error('stderr:', stderr);
+        }
+        const themeDef = themeDefinition.replace("{suffix}", `-${sizeConfig.name}`);
+        const themePathFull = themePath + "/../index.theme";
+        fs.writeFileSync(themePathFull, themeDef, 'utf8');
+    }
 
 })();
